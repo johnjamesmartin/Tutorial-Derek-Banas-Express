b6b00d0fb3bf25c6b4af12ed0c49695b03544652
“John <johnjamesmartin@live.com>
2016-10-07 03:07:01 +0100

    Routing and rendering have been set up

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 var express = require('express');
 var app = express();
 
+
+/* Disable 'x-powered-by' for a more discreet header: */
+
+app.disable('x-powered-by');
+
+
+/* Set Handlebars as is our viewing engine: */
+
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
+/* Use port of host environment or default to port 3000: */
+
 app.set('port', process.env.PORT || 3000);
 
+
+/* Have resources sourced from public directory */
+
+app.use(express.static(__dirname + '/public'));
+
+
+/* Root route: */
+
 app.get('/', function(req, res) {
-    res.send('Express works');
+    res.render('home');
 });
+
+/* Listen for port and offer feedback: */
 
 app.listen(app.get('port'), function() {
     console.log('Express started. Press CTRL + C to terminate')
