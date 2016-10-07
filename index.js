2c2d66de2f5db11a3b3016b1f44be5d949846fd0
“John <johnjamesmartin@live.com>
2016-10-07 20:12:26 +0100

    Form information processing

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var formidable = require('formidable');
+
+var credentials = require('./credentials');
 
 
 /* Disable 'x-powered-by' for a more discreet header: */
@@ -24,6 +27,16 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
 
+/* Body parser allows us to parse encoded data when using POST: */
+
+app.use(require('body-parser').urlencoded({ extended: true }));
+
+
+/* Cookie parser allows us to use cookies: */
+
+app.use(require('cookie-parser')(credentials.cookieSecret));
+
+
 /* Root route: */
 
 app.get('/', function(req, res) {
@@ -35,6 +48,30 @@ app.get('/', function(req, res) {
 
 app.get('/about', function(req, res) {
     res.render('about');
+});
+
+app.get('/contact', function(req, res) {
+    res.render('contact', {
+        csrf: 'CSRF token here'
+    });
+});
+
+
+/* Route to thank user for submitting information: */
+
+app.get('/thankyou', function(req, res) {
+    res.render('thankyou');
+});
+
+
+/* Process POST route: */
+
+app.post('/process', function(req, res) {
+    console.log('Form: ' + req.query.form);
+    console.log('CSRF: ' + req.body._csrf);
+    console.log('Email: ' + req.body.email);
+    console.log('Question: ' + req.body.ques);
+    res.redirect(303, '/thankyou');
 });
 
 
