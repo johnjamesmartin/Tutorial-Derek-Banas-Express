a2cdb09d2353c951daf2d3f8f1342d8ac7330b5d
“John <johnjamesmartin@live.com>
2016-10-07 05:25:51 +0100

    Testing middleware functions

--- index.js
+++ index.js
@@ -30,6 +30,56 @@ app.get('/', function(req, res) {
     res.render('home');
 });
 
+
+/* About route: */
+
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+
+
+/* Middleware function: */
+
+app.use(function(req, res, next) {
+    console.log('Looking for: ' + req.url);
+    next();
+});
+
+
+/* Report and throw errors like so: */
+
+app.get('/junk', function(req, res, next){
+  console.log('Tried to access /junk');
+  throw new Error('/junk does\'t exist');
+});
+
+
+/* Catches the error, log it and continue pipeline: */
+
+app.use(function(err, req, res, next){
+  console.log('Error : ' + err.message);
+  next();
+});
+
+
+/* Handle 404: */
+
+app.use(function(req, res){
+  res.type('text/html');
+  res.status(404);
+  res.render('404');
+});
+
+
+/* Handle 500: */
+
+app.use(function(err, req, res, next){
+  console.log(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+
 /* Listen for port and offer feedback: */
 
 app.listen(app.get('port'), function() {
